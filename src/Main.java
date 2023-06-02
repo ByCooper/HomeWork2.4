a1cdd1a3236a09e966515a2b7ab55594719ee556
ByCooper <kup.official@yahoo.com>
2023-06-02 12:58:30 +0300

    Домашнее задание (решение)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-    }
+        String login = "alpfard1233_sigmund";
+        String password = "signatura_2727old00";
+        String confirmPassword = "signatura_2727old07";
 
-    public static void getAuth(String login, String password, String confirmPassword) {
+        Authorization.getAuthorization(login, password, confirmPassword);
 
     }
 }
